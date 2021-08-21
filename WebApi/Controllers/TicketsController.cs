42b61e2bc4dd7cb7016826b22e962c0a3edede06
Mohamed Adel <mohamedadel2012321@gmail.com>
2021-08-21 03:25:27 +0200

    init

---
+++ WebApi/Controllers/TicketsController.cs
@@ -0,0 +1,86 @@
+﻿using Core.Models;
+using DataStore.EF;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]    
+    public class TicketsController : ControllerBase
+    {
+        private readonly BugsContext db;
+
+        public TicketsController(BugsContext db)
+        {
+            this.db = db;
+        }
+
+
+        [HttpGet]        
+        public IActionResult Get()
+        {           
+            return Ok(db.Tickets.ToList());
+        }
+
+        [HttpGet("{id}")]        
+        public IActionResult GetById(int id)
+        {
+            var ticket = db.Tickets.Find(id);
+            if (ticket == null)
+                return NotFound();
+
+            return Ok(ticket);
+        }
+
+        [HttpPost]        
+        public IActionResult Post([FromBody]Ticket ticket)
+        {
+            db.Tickets.Add(ticket);
+            db.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById),
+                    new { id = ticket.TicketId },
+                    ticket
+                );
+        }        
+
+        [HttpPut("{id}")]        
+        public IActionResult Put(int id, [FromBody] Ticket ticket)
+        {
+            if (id != ticket.TicketId) return BadRequest();
+
+            db.Entry(ticket).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch
+            {
+                if (db.Tickets.Find(id) == null)
+                    return NotFound();
+                throw;
+            }
+            
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]        
+        public IActionResult Delete(int id)
+        {
+            var ticket = db.Tickets.Find(id);
+            if (ticket == null) return NotFound();
+
+            db.Tickets.Remove(ticket);
+            db.SaveChanges();
+
+            return Ok(ticket);
+        }
+
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataStore.EF;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PlatformDemo
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            this._env = env;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (_env.IsDevelopment())
+            {
+                services.AddDbContext<BugsContext>(options =>
+                {
+                    options.UseInMemoryDatabase("Bugs");
+                });
+            }
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BugsContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                //Create the in-memory database for dev environment
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
